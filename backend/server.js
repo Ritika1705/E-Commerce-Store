e12cf906526a56c0c4cac45ce6245d331ce6f627
Ritika Mandal <s.ritika1705@gmail.com>
2024-12-14 16:09:18 +0530

    code updated

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,14 @@ import analyticsRoutes from "./routes/analytics.route.js"
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import  cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-const PORT = 5002;
+const PORT = process.env.PORT || 5002;
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -29,6 +32,13 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
