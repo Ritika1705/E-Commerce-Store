a1c39b0e71c7613b5e80cf178a0d98d2aa1aae61
Ritika Mandal <s.ritika1705@gmail.com>
2024-12-14 16:49:51 +0530

    code updated

--- backend/server.js
+++ backend/server.js
@@ -34,14 +34,15 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
     });
 }
 
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);
+    console.log("Enviroment:" + process.env.NODE_ENV);
     connectDB();
 });
 
